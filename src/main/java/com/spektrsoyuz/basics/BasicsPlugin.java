8546d51b5b239c31f2b5ee7fa9aba6719daab58b
SpektrSoyuz <christies1109@gmail.com>
2025-03-07 11:38:01 -0500

    WIP Add ConfigController

--- src/main/java/com/spektrsoyuz/basics/BasicsPlugin.java
+++ src/main/java/com/spektrsoyuz/basics/BasicsPlugin.java
@@ -1,14 +1,20 @@
 package com.spektrsoyuz.basics;
 
+import com.spektrsoyuz.basics.controller.ConfigController;
 import lombok.Getter;
+import lombok.experimental.Accessors;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
+@Accessors(fluent = true)
 public final class BasicsPlugin extends JavaPlugin {
+
+    private final ConfigController configController = new ConfigController(this);
 
     @Override
     public void onLoad() {
         // Plugin load logic
+        configController.load();
     }
 
     @Override
---
+++ src/main/java/com/spektrsoyuz/basics/controller/ConfigController.java
@@ -0,0 +1,14 @@
+package com.spektrsoyuz.basics.controller;
+
+import com.spektrsoyuz.basics.BasicsPlugin;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ConfigController {
+
+    private final BasicsPlugin plugin;
+
+    public void load() {
+
+    }
+}
