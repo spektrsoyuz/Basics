6932db7670e490f27d1a841b532cc03159b1ea99
SpektrSoyuz <christies1109@gmail.com>
2025-03-07 11:29:12 -0500

    Create Basics plugin

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("java")
+    id("com.gradleup.shadow") version "9.0.0-beta2"
+    id("io.freefair.lombok") version "8.12.2.1"
+}
+
+group = "net.cc"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+}
+
+dependencies {
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("redis.clients:jedis:5.2.0")
+    implementation("org.spongepowered:configurate-hocon:4.1.2")
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("me.clip:placeholderapi:2.11.6")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks {
+    shadowJar {
+        archiveClassifier.set("")
+    }
+    build {
+        dependsOn(shadowJar)
+    }
+}
---
+++ src/main/java/com/spektrsoyuz/basics/BasicsPlugin.java
@@ -0,0 +1,23 @@
+package com.spektrsoyuz.basics;
+
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@Getter
+public final class BasicsPlugin extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        // Plugin load logic
+    }
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
