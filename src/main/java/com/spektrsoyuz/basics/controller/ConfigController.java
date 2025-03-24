1d25b9448eb41e542cbc77b46b4484b0676eed12
SpektrSoyuz <christies1109@gmail.com>
2025-03-24 15:53:45 -0400

    Change tagResolvers list to be mutable

--- src/main/java/com/spektrsoyuz/basics/controller/ConfigController.java
+++ src/main/java/com/spektrsoyuz/basics/controller/ConfigController.java
@@ -15,6 +15,7 @@ import org.spongepowered.configurate.hocon.HoconConfigurationLoader;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -73,7 +74,7 @@ public final class ConfigController {
     public void sendMessage(final Audience audience, final String key, final TagResolver... resolvers) {
         final String message = this.messagesNode.node(key).getString();
 
-        final List<TagResolver> tagResolvers = Arrays.asList(resolvers);
+        final List<TagResolver> tagResolvers = new ArrayList<>(List.of(resolvers));
         tagResolvers.add(Placeholder.parsed("prefix", getPrefix()));
 
         if (this.plugin.getServer().getPluginManager().isPluginEnabled("MiniPlaceholders")) {
